107c614e5ce867c5240fdfb58c4ae665d57a679f
Martin <dhhwang89@gmail.com>
2019-01-30 22:53:46 +0900

    #16 : finish implement __getitem__ and pass lint test

--- data_loader/voc_dataset.py
+++ data_loader/voc_dataset.py
@@ -1,40 +1,49 @@
 import os
-import numpy as np
+from PIL import Image
+
 from torch.utils import data
 
 class VocDetection(data.Dataset):
     """ `PASCAL VOC Detection <http://host.robots.ox.ac.uk/pascal/VOC/>`_ Dataset.
 
     Args:
-        root (string): Root directory where images and annotations were downloaded
-        imageFolder (string) : Path to images folder. image have extension as jpg, jpeg, png
-        annotationFolder (string) : Path to annotation file folder. annotation file have extension as xml
+        root (string):
+            Root directory where images and annotations were downloaded
+        imageFolder (string) :
+            Path to images folder. image have extension as jpg, jpeg, png
+        annotationFolder (string) :
+            Path to annotation file folder. annotation file have extension as xml
         transform (callable, optional): A function/transform that  takes in an PIL image and label
             and returns a transformed version. E.g, ``transforms.ToTensor``
     """
 
-    def __init__(self, root, image_folder="JPEGImages", annotation_folder="Annotations", transform=None):
+    def __init__(self,
+                 root,
+                 image_folder="JPEGImages",
+                 annotation_folder="Annotations",
+                 resize_size=(416, 416)):
 
-        self.root = root
-        self.image_folder = image_folder
-        self.annotation_folder = annotation_folder
+        self.resize_size = resize_size
 
-        self.image_paths = self._get_filepaths(self.image_folder)
-        self.image_paths.sort(key=lambda filepath : int(filepath.split("/")[-1].split(".")[0]))
+        image_path_list = self._get_list_of_filepath(root, image_folder)
+        image_path_list_orderd_by_filename = \
+            sorted(image_path_list,
+                   key=lambda filepath: int(filepath.split("/")[-1].split(".")[0]))
+        self.image_path_list = image_path_list_orderd_by_filename
 
-        self.annotation_paths = self._get_filepaths(self.annotation_folder)
-        self.annotation_paths.sort(key=lambda filepath : int(filepath.split("/")[-1].split(".")[0]))
+        annotation_path_list = self._get_list_of_filepath(root, annotation_folder)
+        annotation_path_list_ordered_by_filename = \
+            sorted(annotation_path_list,
+                   key=lambda filepath: int(filepath.split("/")[-1].split(".")[0]))
+        self.annotation_path_list = annotation_path_list_ordered_by_filename
 
-        classes_ordered_by_alphabet = self._get_classes_list_from_dataset(self.annotation_paths)
-        self.classes = classes_ordered_by_alphabet
+        classes_list = self._get_classes_list_from_dataset(self.annotation_path_list)
+        classes_list_ordered_by_alphabet = sorted(classes_list)
+        self.classes_list = classes_list_ordered_by_alphabet
 
-        self._validate_image_Extension(self.image_paths)
-        assert(self.__is_length_same(image_paths = self.image_paths,
-                                     annotation_paths = self.annotation_paths))
-
-        a = self.__parse_xml_of_voc(self.annotation_paths[0])
-        b = self.__convert_box_label_to_yolo_label(a, self.classes)
-
+        assert self._is_validated_image_Extension(self.image_path_list)
+        assert self._is_validated_annotation_Extension(self.annotation_path_list)
+        assert len(self.image_path_list) == len(self.annotation_path_list)
 
     def __getitem__(self, index):
         """
@@ -44,50 +53,79 @@ class VocDetection(data.Dataset):
         Returns:
             tuple: Tuple (image, target). target is the object returned by ``method``.
         """
-        # TODO it shoould be Implement
+        assert isinstance(index, int)
 
-        pass
+        image = Image.open(self.image_path_list[index]).convert('RGB')
+        image = image.resize(self.resize_size)
+        target = self._get_yolo_label(self.annotation_path_list[index], self.classes_list)
 
-    def _get_classes_list_from_dataset(self, annotation_paths):
+        return image, target
+
+
+    def __len__(self):
+        return len(self.image_path_list)
+
+    @staticmethod
+    def _get_classes_list_from_dataset(annotation_path_list):
         import xml.etree.ElementTree as Et
 
-        assert(len(annotation_paths) != 0)
+        assert isinstance(annotation_path_list, list)
+        for annotation_path in annotation_path_list:
+            assert isinstance(annotation_path, str)
+        assert annotation_path_list
 
-        classes = list()
-        for annotation_path in annotation_paths:
+        classes_list = list()
+        for annotation_path in annotation_path_list:
             xml_file = open(annotation_path, "r")
             tree = Et.parse(xml_file)
+
+            element_list = list()
+            for elem in tree.iter():
+                element_list.append(elem.tag)
+
+            assert "name" in element_list
+
             root = tree.getroot()
 
             objects = root.findall("object")
-            assert(len(objects) != 0)
+            assert objects
 
             for _object in objects:
                 cls_tag = _object.find("name")
-                assert(cls_tag != None)
                 cls = cls_tag.text
-                classes.append(cls) if cls not in classes else None
-                classes = list(filter(None, classes))
+                if cls not in classes_list:
+                    classes_list.append(cls)
 
-        classes_ordered_by_alphabet = sorted(classes)
+                classes_list = list(filter(None, classes_list))
 
-        return classes_ordered_by_alphabet
+        return classes_list
 
-    def _get_label(self):
-        # TODO getting detection label
-        pass
+    def _get_yolo_label(self, annotation_path, classes_list):
+        assert isinstance(annotation_path, str)
+        assert isinstance(classes_list, list)
+        for cls in classes_list:
+            assert isinstance(cls, str)
 
-    def __convert_box_label_to_yolo_label(self, label, classes):
+        box_label = self.__parse_xml_of_voc(annotation_path)
+        yolo_label = self.__convert_box_label_to_yolo_label(box_label, classes_list)
+
+        return yolo_label
+
+    def __convert_box_label_to_yolo_label(self, label, classes_list):
+        assert isinstance(label, dict)
+        assert isinstance(classes_list, list)
+        for cls in classes_list:
+            assert isinstance(cls, str)
 
         root_keys = label.keys()
         size_keys = label["size"].keys()
         number_of_objects = len(label["object"])
 
-        assert ("size" in root_keys)
-        assert ("object" in root_keys)
-        assert ("width" in size_keys)
-        assert ("height" in size_keys)
-        assert (number_of_objects != 0)
+        assert "size" in root_keys
+        assert "object" in root_keys
+        assert "width" in size_keys
+        assert "height" in size_keys
+        assert number_of_objects != 0
 
         yolo_label = list()
 
@@ -98,14 +136,14 @@ class VocDetection(data.Dataset):
 
         for _object in label["object"]:
             _object_keys = _object.keys()
-            assert ("name" in _object_keys)
-            assert ("xmin" in _object_keys)
-            assert ("ymin" in _object_keys)
-            assert ("xmax" in _object_keys)
-            assert ("ymax" in _object_keys)
+            assert "name" in _object_keys
+            assert "xmin" in _object_keys
+            assert "ymin" in _object_keys
+            assert "xmax" in _object_keys
+            assert "ymax" in _object_keys
 
             name = _object["name"]
-            cls = classes.index(name)
+            cls = classes_list.index(name)
             box_coordinate = {
                 "xmin": float(_object["xmin"]),
                 "ymin": float(_object["ymin"]),
@@ -119,35 +157,62 @@ class VocDetection(data.Dataset):
 
         return yolo_label
 
-    def __convert_coordinate(self, image_size, box_coordinate):
-        dw = 1. / image_size["width"]
-        dh = 1. / image_size["height"]
+    @staticmethod
+    def __convert_coordinate(image_size, box_coordinate):
+        image_size_keys = image_size.keys()
+        box_coordinate_keys = box_coordinate.keys()
+        assert "width" in image_size_keys
+        assert "height" in image_size_keys
+        assert "xmin" in box_coordinate_keys
+        assert "ymin" in box_coordinate_keys
+        assert "xmax" in box_coordinate_keys
+        assert "ymax" in box_coordinate_keys
+        assert isinstance(image_size, dict)
+        assert isinstance(box_coordinate, dict)
+        assert isinstance(image_size["width"], float)
+        assert isinstance(image_size["height"], float)
+        assert isinstance(box_coordinate["xmin"], float)
+        assert isinstance(box_coordinate["ymin"], float)
+        assert isinstance(box_coordinate["xmax"], float)
+        assert isinstance(box_coordinate["ymax"], float)
 
         x_of_box = (box_coordinate["xmin"] + box_coordinate["xmax"]) / 2.0
         y_of_box = (box_coordinate["ymin"] + box_coordinate["ymax"]) / 2.0
         width_of_box = box_coordinate["xmax"] - box_coordinate["xmin"]
         height_of_box = box_coordinate["ymax"] - box_coordinate["ymin"]
 
-        relative_x_of_center = x_of_box * dw
-        relative_y_of_center = y_of_box * dh
-        relative_box_width = width_of_box * dw
-        relative_box_height = height_of_box * dh
+        relative_x_of_center = x_of_box / image_size["width"]
+        relative_y_of_center = y_of_box / image_size["height"]
+        relative_box_width = width_of_box / image_size["width"]
+        relative_box_height = height_of_box / image_size["height"]
 
-        yolo_coordinate = [relative_x_of_center,
-                           relative_y_of_center,
-                           relative_box_width,
-                           relative_box_height]
+        return [relative_x_of_center, relative_y_of_center,
+                relative_box_width, relative_box_height]
 
-        return yolo_coordinate
-
-    def __parse_xml_of_voc(self, annotation_path):
+    @staticmethod
+    def __parse_xml_of_voc(annotation_path):
         import xml.etree.ElementTree as Et
-        # [x] TODO it should check about satisfied it have all element for detection
-
-        result = dict()
+        assert isinstance(annotation_path, str)
 
         xml_file = open(annotation_path, "r")
         tree = Et.parse(xml_file)
+
+        element_list = list()
+        for elem in tree.iter():
+            element_list.append(elem.tag)
+
+        assert "size" in element_list
+        assert "width" in element_list
+        assert "height" in element_list
+        assert "object" in element_list
+        assert "name" in element_list
+        assert "bndbox" in element_list
+        assert "xmin" in element_list
+        assert "ymin" in element_list
+        assert "xmax" in element_list
+        assert "ymax" in element_list
+
+        result = dict()
         root = tree.getroot()
 
         size_tag = root.find("size")
@@ -161,97 +226,98 @@ class VocDetection(data.Dataset):
         result["object"] = list()
 
         objects = root.findall("object")
-        assert (len(objects) != 0)
+        assert objects
 
         for _object in objects:
 
-            name_tag = _object.find("name")
-            bndbox_tag = _object.find("bndbox")
-            assert (name_tag != None)
-            assert (bndbox_tag != None)
-
-            xmin_tag = bndbox_tag.find("xmin")
-            ymin_tag = bndbox_tag.find("ymin")
-            xmax_tag = bndbox_tag.find("xmax")
-            ymax_tag = bndbox_tag.find("ymax")
-
-            assert (xmin_tag != None)
-            assert (ymin_tag != None)
-            assert (xmax_tag != None)
-            assert (ymax_tag != None)
-
             result["object"].append({
-                "name": name_tag.text,
-                "xmin": xmin_tag.text,
-                "ymin": ymin_tag.text,
-                "xmax": xmax_tag.text,
-                "ymax": ymax_tag.text
+                "name": _object.find("name").text,
+                "xmin": _object.find("bndbox").find("xmin").text,
+                "ymin": _object.find("bndbox").find("ymin").text,
+                "xmax": _object.find("bndbox").find("xmax").text,
+                "ymax": _object.find("bndbox").find("ymax").text
             })
 
         return result
 
-    def _get_filepaths(self, subdirectory):
-        (dir_path, _, filenames) = next(os.walk(os.path.abspath(os.path.join(self.root, subdirectory))))
+
+    @staticmethod
+    def _get_list_of_filepath(root_directory, sub_directory):
+        assert isinstance(root_directory, str)
+        assert isinstance(sub_directory, str)
+
+        (dir_path, _, filenames) = \
+            next(os.walk(os.path.abspath(os.path.join(root_directory, sub_directory))))
         return [os.path.join(dir_path, filename) for filename in filenames]
 
 
-    def _validate_image_Extension(self, image_paths):
-        # [x] TODO add assertion
-        validations_jpg = self.__validate_extension(image_paths, "jpg")
-        validations_jpeg = self.__validate_extension(image_paths, "jpeg")
-        validations_png = self.__validate_extension(image_paths, "png")
+    def _is_validated_annotation_Extension(self, annotation_path_list):
+        assert isinstance(annotation_path_list, list)
+        for path in annotation_path_list:
+            assert isinstance(path, str)
+
+        validation_list_about_xml = self.__is_validated_extension(annotation_path_list, "xml")
+
+        return self.__is_all_element_validated(validation_list_about_xml)
 
 
-        integration_validation = self.__combine_validations(jpg_validations = validations_jpg,
-                                                            jpeg_validations = validations_jpeg,
-                                                            png_validations = validations_png)
+    def _is_validated_image_Extension(self, image_path_list):
+        assert isinstance(image_path_list, list)
+        for path in image_path_list:
+            assert isinstance(path, str)
 
-        assert(self.__combine_validation_elements(integration_validation))
+        validation_list_about_jpg = self.__is_validated_extension(image_path_list, "jpg")
+        validation_list_about_jpeg = self.__is_validated_extension(image_path_list, "jpeg")
+        validation_list_about_png = self.__is_validated_extension(image_path_list, "png")
 
 
-    def __combine_validation_elements(self, validations):
-        # [x] TODO add assertion
+        integrated_validation_list = \
+            self.__is_supported_image_extension(jpg_validations=validation_list_about_jpg,
+                                                jpeg_validations=validation_list_about_jpeg,
+                                                png_validations=validation_list_about_png)
+
+        return self.__is_all_element_validated(integrated_validation_list)
+
+
+    @staticmethod
+    def __is_all_element_validated(validation_list):
+        assert isinstance(validation_list, list)
+        for validation_value in validation_list:
+            assert isinstance(validation_value, bool)
+
         intergated_validation = True
 
-        for validation in validations:
+        for validation in validation_list:
             intergated_validation = intergated_validation and validation
 
         return intergated_validation
 
 
-    def __combine_validations(self, **kwargs):
-        # [x] TODO add assertion
+    @staticmethod
+    def __is_supported_image_extension(**kwargs):
+        keys = kwargs.keys()
+        assert "jpg_validations" in keys
+        assert "jpeg_validations" in keys
+        assert "png_validations" in keys
+
         validations = list()
-        for key in kwargs.keys():
+        for key in keys:
             validations.append(kwargs[key])
 
+        # Integrate all validation
+        # initial list was selected last of validation list
+        # because of last of validation list not selected for loop
         result = validations[-1]
 
         for i in range(len(validations) - 1):
             result = [result[j] or validations[i][j] for j in range(len(result))]
+
         return result
 
 
-    def __is_length_same(self, **kwargs):
-        # [x] TODO add assertion
-        lengths = list()
-        comparisions = list()
-        flag = True
-        for key in kwargs.keys():
-            lengths.append(len(kwargs[key]))
+    @staticmethod
+    def __is_validated_extension(file_list, extension):
+        assert isinstance(file_list, list)
+        assert isinstance(extension, str)
 
-        for i in range(len(lengths)-1):
-            comparisions.append((lengths[-1] == lengths[i]))
-
-        for comparision in comparisions:
-            flag = flag and comparision
-
-        return flag
-
-
-    def __validate_extension(self, filenames, extension):
-        # [x] TODO add assertion
-        return [filename[-3:].lower() == extension for filename in filenames]
-
-root = "/home/martin/Documents/dev/Deepbaksu_vision/_Datasets/VOC2012/"
-voc = VocDetection(root=root)
+        return [filename[-3:].lower() == extension for filename in file_list]
